09c3d333d774ca35c7897cdf55a3610eed455b50
Pedro Lopez <pit_ter@hotmail.com>
2018-05-12 23:44:25 +0100

    [rick] Improved collisions character with world

--- rick2/src/character.cpp
+++ rick2/src/character.cpp
@@ -34,55 +34,79 @@ Character::~Character() {
 void Character::SetPosX(World* map, int x) {
   // REVISIT: character is currently a single box, this function requires to be updated
   // when the character will be based on a generic player.
+  int tile_weight = map->GetTilesetTileHeight();
+  int character_weight = 8; // REVISIT
+
+  // Compute the displacement in x
+  int desp_x = (x > pos_x) ? (x - pos_x) : (pos_x - x);
 
   // Compute x and y corrections to draw world
-  int tile_col_pos_x, tile_col_x;
-  int tile_col_pos_y, tile_col_y;
-  // Collision moving right
-  if ( x > pos_x) {    
-    tile_col_x = (x + 8 - 1)  / map->GetTilesetTileWidth();
-    tile_col_y = pos_y / map->GetTilesetTileHeight();    
-    if (map->GetTile(tile_col_x, tile_col_y)->GetType() != 308) {
-      pos_x = x;      
-    } else {      
-      pos_x = x - 1;
+  int tile_col_x;
+  int tile_col_up_y   = pos_y / tile_weight;
+  int tile_col_down_y = (pos_y + character_weight - 1) / tile_weight;
+  if ( x > pos_x) {
+    // Collision moving right
+    tile_col_x = (pos_x + desp_x + character_weight)  / tile_weight;
+    if ((map->GetTile(tile_col_x, tile_col_up_y)->GetType() != 308) &&
+        (map->GetTile(tile_col_x, tile_col_down_y)->GetType() != 308)) {
+      // No collision
+      pos_x = pos_x + desp_x;      
+    } else {
+      // Collision. Move to safe position
+      int correction = (pos_x + desp_x + character_weight) % tile_weight;
+      pos_x = pos_x + desp_x - correction;
     }
-  }
-  // Collision moving left
-  if ((x > 0) && (x < pos_x)) {
-    tile_col_x = (x - 1) / map->GetTilesetTileWidth();
-    tile_col_y = pos_y / map->GetTilesetTileHeight();    
-    if (map->GetTile(tile_col_x, tile_col_y)->GetType() != 308)
-      pos_x = x;
-    else
-      pos_x = x + 1;
+  } else if ((x > 0) && (x < pos_x)) {
+    // Collision moving left    
+    tile_col_x = (pos_x - desp_x) / tile_weight;
+    if ((map->GetTile(tile_col_x, tile_col_up_y)->GetType() != 308) &&
+        (map->GetTile(tile_col_x, tile_col_down_y)->GetType() != 308)) {
+      // No collision
+      pos_x = pos_x - desp_x;
+    } else {
+      // Collision. Move to safe position
+      int correction = tile_weight - ((pos_x - desp_x) % tile_weight);
+      pos_x = pos_x - desp_x + correction;      
+    }
   }
 }
 
 void Character::SetPosY(World* map, int y) {
   // REVISIT: character is currently a single box, this function requires to be updated
   // when the character will be based on a generic player.
+  int tile_height = map->GetTilesetTileHeight();
+  int character_height = 8; // REVISIT
+
+  // Compute the displacement in x
+  int desp_y = (y > pos_y) ? (y - pos_y) : (pos_y - y);
 
   // Compute x and y corrections to draw world
-  int tile_col_pos_x, tile_col_x;
-  int tile_col_pos_y, tile_col_y;
-  // Collision moving right
-  if ( y > pos_y) {
-    tile_col_x = pos_x  / map->GetTilesetTileWidth();
-    tile_col_y = (y + 8 - 1) / map->GetTilesetTileHeight();
-    if (map->GetTile(tile_col_x, tile_col_y)->GetType() != 308) {
-      pos_y = y;
-    } else {      
-      pos_y = y - 1;
+  int tile_col_y;
+  int tile_col_up_x   = pos_x / tile_height;
+  int tile_col_down_x = (pos_x + character_height - 1) / tile_height;
+  if (y > pos_y) {
+    // Collision moving down
+    tile_col_y = (pos_y + desp_y + character_height)  / tile_height;
+    if ((map->GetTile(tile_col_up_x, tile_col_y)->GetType() != 308) &&
+        (map->GetTile(tile_col_down_x, tile_col_y)->GetType() != 308)) {
+      // No collision
+      pos_y = pos_y + desp_y;
+    } else {
+      // Collision. Move to safe position
+      int correction = (pos_y + desp_y + character_height) % tile_height;
+      pos_y = pos_y + desp_y - correction;
     }
-  }
-  // Collision moving left
-  if ((y > 0) && (y < pos_y)) {
-    tile_col_x = pos_x / map->GetTilesetTileWidth();
-    tile_col_y = (y - 1) / map->GetTilesetTileHeight();    
-    if (map->GetTile(tile_col_x, tile_col_y)->GetType() != 308)
-      pos_y = y;
-    else
-      pos_y = y + 1;
+  } else if ((y > 0) && (y < pos_y)) {
+    // Collision moving up    
+    tile_col_y = (pos_y - desp_y) / tile_height;
+    if ((map->GetTile(tile_col_up_x, tile_col_y)->GetType() != 308) &&
+        (map->GetTile(tile_col_down_x, tile_col_y)->GetType() != 308)) {
+      // No collision
+      pos_y = pos_y - desp_y;
+    } else {
+      // Collision. Move to safe position
+      int correction = tile_height - ((pos_y - desp_y) % tile_height);
+      pos_y = pos_y - desp_y + correction;
+    }
   }
 }
